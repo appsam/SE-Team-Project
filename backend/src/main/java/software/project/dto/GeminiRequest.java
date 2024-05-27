3fe3c2933901ab21b9a00b7379fcbcfabc9f5d06
MS <rty7854@naver.com>
2024-05-27 11:06:14 +0900

    챗봇 구현

---
+++ backend/src/main/java/software/project/dto/GeminiRequest.java
@@ -0,0 +1,57 @@
+package software.project.dto;
+
+import java.util.List;
+
+public class GeminiRequest {
+    private List<Content> contents;
+
+    public static class Content {
+        private List<Part> parts;
+
+        public static class Part {
+            private String text;
+
+            public Part() {}
+
+            public Part(String text) {
+                this.text = text;
+            }
+
+            public String getText() {
+                return text;
+            }
+
+            public void setText(String text) {
+                this.text = text;
+            }
+        }
+
+        public Content() {}
+
+        public Content(List<Part> parts) {
+            this.parts = parts;
+        }
+
+        public List<Part> getParts() {
+            return parts;
+        }
+
+        public void setParts(List<Part> parts) {
+            this.parts = parts;
+        }
+    }
+
+    public GeminiRequest() {}
+
+    public GeminiRequest(List<Content> contents) {
+        this.contents = contents;
+    }
+
+    public List<Content> getContents() {
+        return contents;
+    }
+
+    public void setContents(List<Content> contents) {
+        this.contents = contents;
+    }
+}
